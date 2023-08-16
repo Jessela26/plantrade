1acd3f2df7987a23d37e14cb1e3a10b748e01ab0
Jessela26 <jelbaniqued26@gmail.com>
2023-08-15 22:18:25 -0400

    initial commit

---
+++ plantrade/plantrade/plantradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantradeApp.swift
+//  plantrade
+//
+//  Created by Jessela Emile on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct plantradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
